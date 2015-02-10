8ba2d72c9e3f0ace952077c8d6c0e08c9e6598b4
Pekka Heikura <pekkah@gmail.com>
2015-02-10 19:35:33 +0200

    Update dependencies to latest

---
+++ src/FileSystem.WebApiSample/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Tanka.FileSystem.WebApiSample.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ src/FileSystem.WebApiSample/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace Tanka.FileSystem.WebApiSample.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
